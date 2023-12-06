783dc9b580ab62f13d7301e529daf803ca542e77
Slot <s1059819898@gmail.com>
2023-12-07 02:20:27 +0800

    添加 日志工具类

---
+++ app/src/main/java/com/slot/twostepverification/utils/LogUtils.kt
@@ -0,0 +1,39 @@
+package com.slot.twostepverification.utils
+
+import android.util.Log
+import com.slot.twostepverification.BuildConfig
+import com.slot.twostepverification.const.LocalConfig
+import splitties.init.appCtx
+
+object AppLog {
+    private val mLogs = arrayListOf<Triple<Long, String, Throwable?>>()
+
+    val logs get() = mLogs.toList()
+
+    @Synchronized
+    fun put(message: String?, throwable: Throwable? = null, toast: Boolean = false) {
+        message ?: return
+        if (toast) {
+            appCtx.showToasts(message)
+        }
+        if (mLogs.size > 100) {
+            mLogs.removeLastOrNull()
+        }
+        mLogs.add(0, Triple(System.currentTimeMillis(), message, throwable))
+        if (BuildConfig.DEBUG) {
+            val stackTrace = Thread.currentThread().stackTrace
+            Log.e(stackTrace[3].className, message, throwable)
+        }
+    }
+
+    @Synchronized
+    fun clear() {
+        mLogs.clear()
+    }
+
+    fun putDebug(message: String?, throwable: Throwable? = null) {
+        if (LocalConfig.recordLog) {
+            put(message, throwable)
+        }
+    }
+}
