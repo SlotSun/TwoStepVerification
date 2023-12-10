ff20241a9af14ec180e676827af44a9892e08dc0
Slot <s1059819898@gmail.com>
2023-12-10 20:46:44 +0800

    修复 无法去重

--- app/src/main/java/com/slot/twostepverification/data/entity/VerificationItem.kt
+++ app/src/main/java/com/slot/twostepverification/data/entity/VerificationItem.kt
@@ -14,4 +14,39 @@ data class VerificationItem(
     val sha:String? = "SHA1",
     val counter:Int? = 0,
     val used:Int? = 0
-):Parcelable
+):Parcelable {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as VerificationItem
+
+        if (type != other.type) return false
+        if (name != other.name) return false
+        if (vendor != other.vendor) return false
+        if (key != null) {
+            if (other.key == null) return false
+            if (!key.contentEquals(other.key)) return false
+        } else if (other.key != null) return false
+        if (time != other.time) return false
+        if (length != other.length) return false
+        if (sha != other.sha) return false
+        if (counter != other.counter) return false
+        if (used != other.used) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = type?.hashCode() ?: 0
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (vendor?.hashCode() ?: 0)
+        result = 31 * result + (key?.contentHashCode() ?: 0)
+        result = 31 * result + (time ?: 0)
+        result = 31 * result + (length ?: 0)
+        result = 31 * result + (sha?.hashCode() ?: 0)
+        result = 31 * result + (counter ?: 0)
+        result = 31 * result + (used ?: 0)
+        return result
+    }
+}
